980cce124e5604038133b9d6a80f9de0896f7f57
laxmishahpeti <laxmishahpeti@gmail.com>
2024-03-13 22:49:08 +0530

    singUp2

--- singUp.java
+++ singUp.java
@@ -1,27 +1,24 @@
+
 package votingSystem;
 import java.util.Scanner;
-
-public class signIn {
-	public int signInPassword;
-	public String signInName;
+public class signUp {
+	public String name1,name2;
+	public int voteId;
+	public int birthYear,password;
 	Scanner sc=new Scanner(System.in);
-	signUp signup=new signUp();
-	public void signInInput() {
-		System.out.println("NAME AND PASSWORD VERIFICATION");
-		System.out.println("\nEnter correct Details:");
-		System.out.println("Enter Name:");
-		signInName=sc.next();
-		System.out.println("Enter password:");
-		signInPassword=sc.nextInt();
+	public void inputSignUpdetails() {
+		System.out.println("-----------WELCOME TO ONLINE VOTING SYSTEM-----------");
+		
+		System.out.println("\nSIGN IN WITH YOUR DETAILS");
+		System.out.println("Enter Your name:");
+		name1=sc.next();
+		name2=name1;
+		System.out.println("Enter your voting ID:");
+		voteId=sc.nextInt();
+		System.out.println("Enter your Birth Year:(Remember it)");
+		birthYear=sc.nextInt();
+		password=birthYear;
+		
 	}
-	public void checkCorrectSignup() {
-	if((signInPassword==signup.password)&&(signInName==signup.name2)){
-		return;
-	}
-	else {
-		signInInput();
-	}
-	System.out.println("Corrected entered information");
+	
 }
-}
-
