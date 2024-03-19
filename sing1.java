c4a5657d9d624fb13b7a870d0e52c6ea81736bc4
laxmishahpeti <laxmishahpeti@gmail.com>
2024-03-19 19:55:11 +0530

    java

---
+++ sing1.java
@@ -0,0 +1,27 @@
+package votingSystem;
+import java.util.Scanner;
+
+public class signIn {
+	
+	public int signInPassword;
+	public String signInName;
+	Scanner sc=new Scanner(System.in);
+	signUp signup=new signUp();
+	public void signInInput() {
+		System.out.println("NAME AND PASSWORD VERIFICATION");
+		System.out.println("\nEnter correct Details:");
+		System.out.println("Enter Name:");
+		signInName=sc.next();
+		System.out.println("Enter password:");
+		signInPassword=sc.nextInt();
+	}
+	public void checkCorrectSignup() {
+	if((signInPassword==signup.password)&&(signInName==signup.name2)){
+		return;
+	}
+	else {
+		signInInput();
+	}
+	System.out.println("Corrected entered information");
+}
+}
